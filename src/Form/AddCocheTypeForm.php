9760263acf7c550e87a6d1257f5eb6eb7186df17
Charlygoku <152430771+Charlygoku@users.noreply.github.com>
2025-06-05 23:56:46 +0200

    añadir retriciones

--- src/Form/AddCocheTypeForm.php
+++ src/Form/AddCocheTypeForm.php
@@ -7,7 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class AddCocheTypeForm extends AbstractType
 {
@@ -16,15 +16,57 @@ class AddCocheTypeForm extends AbstractType
         $builder
             ->add('matricula', TextType::class, [
                 'label' => 'Matrícula del coche',
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'La matrícula no puede estar vacía.',
+                    ]),
+                    new Assert\Length([
+                        'min' => 4,
+                        'max' => 10,
+                        'minMessage' => 'La matrícula debe tener al menos {{ limit }} caracteres.',
+                        'maxMessage' => 'La matrícula no puede tener más de {{ limit }} caracteres.',
+                    ]),
+                    new Assert\Regex([
+                        'pattern' => '/^[0-9]{4}[A-Z]{3}$/i',
+                        'message' => 'La matrícula debe tener el formato 1234ABC.',
+                    ]),
+                ],
             ])
             ->add('marca', TextType::class, [
                 'label' => 'Marca del coche',
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'La marca no puede estar vacía.',
+                    ]),
+                    new Assert\Length([
+                        'max' => 50,
+                        'maxMessage' => 'La marca no puede tener más de {{ limit }} caracteres.',
+                    ]),
+                ],
             ])
             ->add('modelo', TextType::class, [
                 'label' => 'Modelo del coche',
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'El modelo no puede estar vacío.',
+                    ]),
+                    new Assert\Length([
+                        'max' => 50,
+                        'maxMessage' => 'El modelo no puede tener más de {{ limit }} caracteres.',
+                    ]),
+                ],
             ])
             ->add('color', TextType::class, [
                 'label' => 'Color del coche',
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'El color no puede estar vacío.',
+                    ]),
+                    new Assert\Length([
+                        'max' => 30,
+                        'maxMessage' => 'El color no puede tener más de {{ limit }} caracteres.',
+                    ]),
+                ],
             ]);
     }
 
